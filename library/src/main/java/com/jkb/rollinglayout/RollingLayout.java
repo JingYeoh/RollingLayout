c0ec12f85375fc100ea486cb421901aed4a51c6b
JustKiddingBaby <yangjing9611@foxmail.com>
2017-07-27 14:57:23 +0800

    alter RollingLayout's parent class.

--- library/src/main/java/com/jkb/rollinglayout/RollingLayout.java
+++ library/src/main/java/com/jkb/rollinglayout/RollingLayout.java
@@ -2,18 +2,11 @@ package com.jkb.rollinglayout;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
-import android.widget.Scroller;
-
-import java.lang.ref.WeakReference;
+import android.widget.ViewFlipper;
 
 /**
  * A view that can rolling automatic within child views list.
@@ -22,7 +15,7 @@ import java.lang.ref.WeakReference;
  * Created by yangjing on 17-7-21.
  */
 
-public class RollingLayout extends FrameLayout implements RollingLayoutAction {
+public class RollingLayout extends ViewFlipper implements RollingLayoutAction {
 
     private static final String TAG = "RollingLayout";
     //attrs
@@ -31,13 +24,6 @@ public class RollingLayout extends FrameLayout implements RollingLayoutAction {
     private int mPauseTime;
 
     //data
-    private static final int WHAT_ROLLING_NEXT = 1001;
-    private static final int WHAT_ROLLING_STOP = 1002;
-    private static final int WHAT_REQUEST_LAYOUT = 2001;
-    private int mCurrentPosition = 0;
-    private boolean isAllowScroll = false;
-    private RollingHandler rollingHandler;
-    private Scroller mScroller;
 
     //listener
     private OnRollingChangedListener onRollingChangedListener;
@@ -49,13 +35,8 @@ public class RollingLayout extends FrameLayout implements RollingLayoutAction {
     }
 
     public RollingLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public RollingLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        super(context, attrs);
         initAttrs(attrs);
-        mScroller = new Scroller(getContext());
     }
 
     /**
@@ -101,184 +82,14 @@ public class RollingLayout extends FrameLayout implements RollingLayoutAction {
 
     @Override
     public void startRolling() {
-        mCurrentPosition = 0;
-        if (getChildCount() == 0) {
-            throw new RuntimeException("the RollingLayout must host one child view at least");
-        }
-        if (rollingHandler == null) {
-            rollingHandler = new RollingHandler(this);
-        }
-        rollingHandler.sendEmptyMessageDelayed(WHAT_ROLLING_NEXT, mPauseTime);
     }
 
     @Override
     public void stopRolling() {
-        if (rollingHandler != null) {
-            rollingHandler.sendEmptyMessage(WHAT_ROLLING_STOP);
-        }
     }
 
     @Override
     public void addOnRollingChangedListener(@NonNull OnRollingChangedListener onRollingChangedListener) {
         this.onRollingChangedListener = onRollingChangedListener;
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
-
-        //measure the view's width.
-        int widthResult = 0;
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        if (widthMode == MeasureSpec.EXACTLY) {
-            widthResult = widthSize;
-        } else {
-            widthResult = getMaxChildViewWidth();
-        }
-        //measure the view's height.
-        int heightResult = 0;
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-        if (heightMode == MeasureSpec.EXACTLY) {
-            heightResult = heightSize;
-        } else {
-            heightResult = getMaxChildViewHeight();
-        }
-
-        setMeasuredDimension(widthResult, heightResult);
-    }
-
-    /**
-     * traverse the child views and return the max width.
-     *
-     * @return max width of child view.
-     */
-    public int getMaxChildViewWidth() {
-        int maxWidth = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View childView = getChildAt(i);
-            int width = childView.getMeasuredWidth()
-                    + childView.getPaddingLeft() + childView.getPaddingRight();
-            maxWidth = width > maxWidth ? width : maxWidth;
-        }
-        return maxWidth;
-    }
-
-    /**
-     * traverse the child views and return the max height.
-     *
-     * @return max height of child view.
-     */
-    public int getMaxChildViewHeight() {
-        int maxHeight = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View childView = getChildAt(i);
-            int height = childView.getMeasuredHeight() +
-                    +childView.getPaddingTop() + childView.getPaddingBottom();
-            maxHeight = height > maxHeight ? height : maxHeight;
-        }
-        return maxHeight;
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-
-        int viewHeight = getMeasuredHeight();
-        int viewWidth = getMeasuredWidth();
-
-        for (int i = 0; i < getChildCount(); i++) {
-            View childView = getChildAt(i);
-            int currentChildHeight = i * getMeasuredHeight();
-            int currentChildWidth = i * getMeasuredWidth();
-
-            int childLeft;
-            int childRight;
-            int childTop;
-            int childBottom;
-
-            //make child view support padding attributes.
-            int childHeight = childView.getMeasuredHeight()
-                    + childView.getPaddingTop() + childView.getPaddingBottom();
-            int childWidth = childView.getMeasuredWidth()
-                    + childView.getPaddingLeft() + childView.getPaddingRight();
-
-            switch (mOrientation) {
-                case UP_DOWN:
-                case DOWN_UP:
-                    childTop = currentChildHeight + (viewHeight - childHeight) / 2;
-                    childBottom = childTop + childHeight;
-                    childView.layout(getPaddingLeft(), childTop, viewWidth - getPaddingRight(), childBottom);
-                    break;
-                case LEFT_RIGHT:
-                case RIGHT_LEFT:
-                    childLeft = currentChildWidth + (viewWidth - childWidth) / 2;
-                    childRight = childLeft + childWidth;
-                    childView.layout(childLeft, getPaddingTop(), childRight, viewHeight - getPaddingBottom());
-                    break;
-            }
-        }
-
-        if (rollingHandler != null && isAllowScroll) {
-            rollingHandler.sendEmptyMessageDelayed(WHAT_ROLLING_NEXT, mPauseTime);
-        }
-    }
-
-    private void smoothScrollTo(int destX, int destY) {
-        int scrollY = getScrollY();
-        int deltaY = destY - scrollY;
-        mScroller.startScroll(0, scrollY, destX, deltaY, mEachTime);
-        invalidate();
-    }
-
-    @Override
-    public void computeScroll() {
-        super.computeScroll();
-        if (mScroller.computeScrollOffset()) {
-            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
-            invalidate();
-        } else {
-            if (onRollingChangedListener != null) {
-                onRollingChangedListener.onRollingChanged(mCurrentPosition, getChildCount());
-            }
-            if (mCurrentPosition == getChildCount()) {
-//                rollingHandler.sendEmptyMessage(WHAT_REQUEST_LAYOUT);
-            } else if (isAllowScroll) {
-                rollingHandler.sendEmptyMessageDelayed(WHAT_ROLLING_NEXT, mPauseTime);
-            }
-        }
-    }
-
-    /**
-     * Inside handler for loop rolling and keep out of leaks.
-     */
-    private static class RollingHandler extends Handler {
-        private final WeakReference<RollingLayout> mRollingLayout;
-
-        RollingHandler(RollingLayout rollingLayout) {
-            mRollingLayout = new WeakReference<>(rollingLayout);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            RollingLayout rollingLayout = mRollingLayout.get();
-            if (rollingLayout == null) return;
-            switch (msg.what) {
-                case WHAT_REQUEST_LAYOUT:
-                    /*rollingLayout.mCurrentPosition = 0;
-                    rollingLayout.requestLayout();*/
-                    break;
-                case WHAT_ROLLING_NEXT:
-                    rollingLayout.isAllowScroll = true;
-                    rollingLayout.mCurrentPosition++;
-                    rollingLayout.mCurrentPosition %= rollingLayout.getChildCount();
-                    rollingLayout.smoothScrollTo(0, rollingLayout.mCurrentPosition * rollingLayout.getMeasuredHeight());
-                    break;
-                case WHAT_ROLLING_STOP:
-                    rollingLayout.isAllowScroll = false;
-                    break;
-            }
-        }
     }
 }
