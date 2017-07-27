a2cf35af1dabf1abf1509dcb113e4b1e94b56e79
JingYeoh <yangjing9611@foxmail.com>
2017-07-27 23:09:46 +0800

    add for animators for RollingLayout.

--- library/src/main/java/com/jkb/rollinglayout/RollingLayout.java
+++ library/src/main/java/com/jkb/rollinglayout/RollingLayout.java
@@ -2,9 +2,12 @@ package com.jkb.rollinglayout;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.annotation.AnimRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.ViewFlipper;
 
@@ -19,6 +22,7 @@ public class RollingLayout extends ViewFlipper implements RollingLayoutAction {
 
     private static final String TAG = "RollingLayout";
     //attrs
+    @Orientation
     private int mOrientation;
     private int mEachTime;
     private int mPauseTime;
@@ -49,24 +53,44 @@ public class RollingLayout extends ViewFlipper implements RollingLayoutAction {
         mEachTime = ta.getInteger(R.styleable.RollingLayout_rolling_eachTime, 500);
         mPauseTime = ta.getInteger(R.styleable.RollingLayout_rolling_pause, 1000);
         ta.recycle();
+
+        setRollingPauseTime(mPauseTime);
+        setRollingEachTime(mEachTime);
+        setRollingOrientation(mOrientation);
     }
 
     @Override
     public void setRollingOrientation(@Orientation int orientation) {
         mOrientation = orientation;
-        postInvalidate();
+        switch (orientation) {
+            case UP_DOWN:
+                setInAnimation(getAnimator(R.anim.slide_up_to_down_in));
+                setOutAnimation(getAnimator(R.anim.slide_up_to_down_out));
+                break;
+            case DOWN_UP:
+                setInAnimation(getAnimator(R.anim.slide_down_to_up_in));
+                setOutAnimation(getAnimator(R.anim.slide_down_to_up_out));
+                break;
+            case LEFT_RIGHT:
+                setInAnimation(getAnimator(R.anim.slide_left_to_right_in));
+                setOutAnimation(getAnimator(R.anim.slide_left_to_right_out));
+                break;
+            case RIGHT_LEFT:
+                setInAnimation(getAnimator(R.anim.slide_right_to_left_in));
+                setOutAnimation(getAnimator(R.anim.slide_right_to_left_out));
+                break;
+        }
     }
 
     @Override
     public void setRollingEachTime(int time) {
         mEachTime = time;
-        postInvalidate();
     }
 
     @Override
     public void setRollingPauseTime(int time) {
         mPauseTime = time;
-        postInvalidate();
+        setFlipInterval(time);
     }
 
     @Override
@@ -82,14 +106,28 @@ public class RollingLayout extends ViewFlipper implements RollingLayoutAction {
 
     @Override
     public void startRolling() {
+        startFlipping();
     }
 
     @Override
     public void stopRolling() {
+        stopFlipping();
     }
 
     @Override
     public void addOnRollingChangedListener(@NonNull OnRollingChangedListener onRollingChangedListener) {
         this.onRollingChangedListener = onRollingChangedListener;
+    }
+
+    /**
+     * return Animator and set duration time.
+     *
+     * @param animId the resource id of animator.
+     * @return {@link Animation}
+     */
+    private Animation getAnimator(@AnimRes int animId) {
+        Animation animation = AnimationUtils.loadAnimation(getContext(), animId);
+        animation.setDuration(mEachTime);
+        return animation;
     }
 }
